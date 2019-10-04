e2e1ed67b2c67c322fcd22e44d2ef2477b2e7d66
Dominic Doty <doty.dominic@gmail.com>
2019-10-03 21:08:32 -0600

    baseline functions for grabbing courses, quizzes, and assignments

--- canvas_warrior.py
+++ canvas_warrior.py
@@ -5,12 +5,47 @@
 import requests
 import json
 
-api_token = '' # TODO: Add a way to get this from a command line arg or something for now
-api_url_base = 'http://canvas.example.com/api/v1/'
+api_token =  # TODO: Add a way to get this from a command line arg or something for now
+api_url_base = 'http://canvas.colorado.edu/api/v1/'
 
-
-headers = {'Content-Type': 'application/json',
+headers = { 'Content-Type': 'application/json',
             'Authorization': 'Bearer {0}'.format(api_token)}
+
+def get_courses():
+    api_url = '{0}courses'.format(api_url_base)
+    courses = requests.get(api_url, headers=headers)
+
+    if courses.status_code == 200:
+        return json.loads(courses.content.decode('utf-8'))
+    else:
+        return None
+
+
+def get_assignments(course_id):
+    api_url = '{0}courses/{1}/assignments'.format(api_url_base, course_id)
+    courses = requests.get(api_url, headers=headers)
+
+    if courses.status_code == 200:
+        return json.loads(courses.content.decode('utf-8'))
+    else:
+        return None
+
+
+def get_quizzes(course_id):
+    api_url = '{0}courses/{1}/quizzes'.format(api_url_base, course_id)
+    courses = requests.get(api_url, headers=headers)
+
+    if courses.status_code == 200:
+        return json.loads(courses.content.decode('utf-8'))
+    else:
+        return None
+
+
+courses = get_courses()
+print(courses.id)
+# for k, v in courses['course_id'].items():
+    # print('{0}:{1}'.format(k,v))
+
 
 # example request
 # curl https://<canvas>/api/v1/courses/<course_id>/quizzes \
