18b8001051b72b67ab86f1ad47909e06038f55aa
Dominic Doty <doty.dominic@gmail.com>
2019-10-07 17:32:38 -0600

    working. need better date system, to parallelize it, and add OAUTH

--- canvas_warrior.py
+++ canvas_warrior.py
@@ -5,15 +5,22 @@
 import requests
 import json
 import subprocess
+import time
 from packaging import version
 from datetime import datetime
+from pathlib import Path
+
+
+#####################################
+##          COMMON JUNK
+#####################################
+
 
 # Authentication (currently using user generated token)
 # TODO: Switch this to OAUTH
-from pathlib import Path
 home = str(Path.home())
 try:
-    f=open("{0}/canvas_access.token".format(home), "r")
+    f=open("{0}/.task/canvas_access.token".format(home), "r")
 except:
     print('FATAL: api_token not found or no read permissions in {0}/canvas_access.token'.format(home))
     exit()
@@ -22,6 +29,8 @@ else:
         api_token = f.read().rstrip()
         [line.rstrip('\n') for line in api_token]
         print('api_token = {0}'.format(api_token))
+
+    f.close()
 
 # Common URL to all requests
 api_url_base = 'http://canvas.colorado.edu/api/v1/'
@@ -32,8 +41,11 @@ headers = { 'Content-Type': 'application/json',
             'Authorization': 'Bearer {0}'.format(api_token)}
 
 
+#####################################
+##          FUNCTIONS
+#####################################
+
 # Check if taskwarrior version is greater than or equal to some version
-# the script may not work for versions older than this
 def check_taskwarrior_version(check_version):
     try:
         process_result = subprocess.run(["task", "--version"], capture_output=True, timeout=2, encoding='utf-8')
@@ -44,7 +56,7 @@ def check_taskwarrior_version(check_version):
             print("WARNING: taskwarrior version is older than the one used to develop this script. Behavior isn't guaranteed.")
 
 
-# Get all the tasks in taskwarrior
+# Returns JSON of all pending and waiting tasks
 def tasks_fetch():
     try:
         task_proc = subprocess.run(["task", "status:pending", "or", "status:waiting", "export"], capture_output=True, timeout=2)
@@ -53,6 +65,38 @@ def tasks_fetch():
         exit()
     else:
         return json.loads(task_proc.stdout)
+
+
+# Returns a dict of associations between course id and project tag
+def projects_fetch():
+    home = str(Path.home())
+    try:
+        f=open("{0}/.task/canvas.json".format(home), "r")
+    except:
+        print("WARNING: No 'canvas.json' config found in '~/.task'")
+        return_value = None
+    else: 
+        if f.mode == 'r':
+            projects_raw = f.read().rstrip()
+            return_value = json.loads(projects_raw)
+        else:
+            print("ERROR: Cannot read 'canvas.json' in '~/.task'")    
+            return_value = None
+
+    f.close()
+    return return_value
+
+
+# Writes json of associations between course id and project tag to canvas.json
+def projects_put(projects_associations):
+    home = str(Path.home())
+    try:
+        f=open("{0}/.task/canvas.json".format(home), "w")
+    except:
+        print("WARNING: Failed to write 'canvas.json' config in '~/.task'")
+    else: 
+        f.write(json.dumps(projects_associations))
+    f.close()
 
 
 # Recursive API GET, following header links as long as 'next' exists
@@ -66,7 +110,8 @@ def recursive_api_fetch(url, headers):
             next_data = recursive_api_fetch(response.links['next']['url'], headers)
             data_set = data_set + next_data
     else:
-        data_set = None
+        time.sleep(.2)
+        return recursive_api_fetch(url, headers=headers)
 
     return data_set
 
@@ -111,8 +156,7 @@ def assignment_submitted(assignment):
     course_id = assignment['course_id']
     assignment_id = assignment['id']
     url = '{0}courses/{1}/assignments/{2}/submissions/self'.format(api_url_base, course_id, assignment_id)
-    response = requests.get(url, headers=headers)
-    submission = json.loads(response.content.decode('utf-8'))
+    submission = recursive_api_fetch(url, headers=headers)
 
     if submission['workflow_state'] == "unsubmitted":
         return False
@@ -134,20 +178,36 @@ def task_exists(assignment, taskwarrior_data):
 
 # Returns 'task import' ready JSON of an assignment
 def create_task(assignment):
-    canv_uuid = "C" + str(assignment['course_id']) + "A" + str(assignment['id'])
+    canv_course_id = str(assignment['course_id'])
+    project_tag = project_associations.get(canv_course_id)
+    canv_assign_id = str(assignment['id'])
+    canv_uuid = "C" + canv_course_id + "A" + canv_assign_id
     canv_name = assignment['name']
-    canv_due_date = datetime.strptime(assignment['due_at'], '%Y-%m-%dT%H:%M:%SZ')
-    canv_wait_date = datetime.strptime(assignment['unlock_at'], '%Y-%m-%dT%H:%M:%SZ')
+
+    if assignment['due_at'] == None:
+        canv_due_date = datetime.today()
+    else:
+        canv_due_date = datetime.strptime(assignment['due_at'], '%Y-%m-%dT%H:%M:%SZ')
+
+    if assignment['due_at'] == None:
+        canv_wait_date = datetime.today()
+    else:
+        canv_wait_date = datetime.strptime(assignment['unlock_at'], '%Y-%m-%dT%H:%M:%SZ')
+
     task = {}
 
     # Get User Inputs or take defaults
     task['description'] = input("Description [{0}]: ".format(canv_name)) or canv_name
-    task['project'] = input("Project: ") or None
+    task['project'] = input("Project [{0}]: ".format(project_tag)) or project_tag
     task['canvas'] = canv_uuid
-
     task['due'] = input("Due Date [{0}]: ".format(canv_due_date)) or datetime.strftime(canv_due_date, '%Y-%m-%d %H:%M:%S')
     task['wait'] = input("Wait Date [{0}]: ".format(canv_wait_date)) or datetime.strftime(canv_wait_date, '%Y-%m-%d %H:%M:%S')
 
+    # Check for Changing Course-Project Associations and Update Config File
+    if task['project'] != project_tag:
+        project_associations[canv_course_id] = task['project']
+
+    # TODO: Handle this with taskwarrior date styles (friday, today, etc) cause this stinks
     # Reformat User Date Overrides
     task['due'] = datetime.strptime(task['due'], '%Y-%m-%d %H:%M:%S')
     task['wait'] = datetime.strptime(task['wait'], '%Y-%m-%d %H:%M:%S')
@@ -156,17 +216,22 @@ def create_task(assignment):
     
     return task
 
-# Main Work
+
+#####################################
+##          MAIN
+#####################################
+
 check_taskwarrior_version("2.5.1")
+project_associations = projects_fetch()
 task_json = tasks_fetch()
 courses = get_courses()
 import_tasks = []
+
 # print(json.dumps(task_json, indent=2, separators=(',', ': ')))
 
+
 # Iterate Over Courses and Get Assignments
-
-del courses[1:]
-
+# TODO: Parallelize all this to hide API latency
 for course in courses:
     if student_enrolled(course):
         assignments = get_assignments(course)
@@ -181,10 +246,9 @@ for course in courses:
                     # Not yet due + not submitted, add
                     print("add: ", assignment['name'])
                     import_tasks.append(create_task(assignment))
-                else:
-                    # Overdue or submitted, no task exists
-                    print("ignore: ", assignment['name'])
 
-print(json.dumps(import_tasks, indent=2, separators=(',', ': ')))
-# print("\t", assignment['name'], "\t", assignment['unlock_at'], " ", assignment['due_at'], " ", assignment['id'])
+# Check for changes to project associations and rewrite if so
+projects_put(project_associations)
 
+# Add tasks to taskwarrior
+subprocess.run(["task", "import"], input=json.dumps(import_tasks), encoding='utf-8')
